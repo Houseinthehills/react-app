c956c9352fb455cf457693332b75ba9b1ac966e9
Derrick <sipho.mphurpi@up.ac.za>
2022-06-14 12:45:41 +0200

    Updates

---
+++ precache-manifest.09b51e101772a5fe8369f38b6d4e8c70.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d3ff04ea08efb44ae62",
+    "url": "/react-app/static/css/main.7e495075.chunk.css"
+  },
+  {
+    "revision": "0d3ff04ea08efb44ae62",
+    "url": "/react-app/static/js/main.0d3ff04e.chunk.js"
+  },
+  {
+    "revision": "c599289ce5f502c935f5",
+    "url": "/react-app/static/js/1.c599289c.chunk.js"
+  },
+  {
+    "revision": "ec46125784e0b1f6cca6",
+    "url": "/react-app/static/js/runtime~main.ec461257.js"
+  },
+  {
+    "revision": "244248488466da92dc58ce30cf2c7d7c",
+    "url": "/react-app/static/media/slider.24424848.jpg"
+  },
+  {
+    "revision": "ea4fcc46a31c8924994d53f1e316b957",
+    "url": "/react-app/static/media/slider-2.ea4fcc46.jpg"
+  },
+  {
+    "revision": "609adbc7825a839e3a00b76d6049ee44",
+    "url": "/react-app/static/media/slider-3.609adbc7.jpg"
+  },
+  {
+    "revision": "bfc0d0b2919e567049a481fa2e287efa",
+    "url": "/react-app/index.html"
+  }
+];
